98fb94c421f618b078f720df8c98b039f0f132ab
Никита Каширин <kashirin.nikita@yandex.ru>
2024-01-20 12:27:16 +0700

    To review

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# Проект парсинга pep
+## Парсер документации Python
+### Проект представляет собой парсер:
+- статусов PEP
+- автора, заголовка и ссылки на статьи в разделах 'What’s New in Python'
+- ссылки на документацию, версии и статусы Python
+- ссылки на документацию актуальной версии с последующем её скачиванием    
+    
+Так же имеется возможность вывода данных поиска в виде таблицы(или построчно) в консоль и файл формата CSV.    
+
+## Как запустить проект:
+### Клонировать репозиторий и перейти в него в командной строке:
+```
+git clone git@github.com:hazardchik/bs4_parser_pep.git
+```
+
+### Создать и активировать виртуальное окружение:
+```
+python -m venv venv
+source venv/Scripts/activate
+```
+
+### Установить зависимости из файла requirements.txt:
+```
+pip install -r requirements.txt
+```
+
+### Команды:
+```
+usage: main.py [-h] [-c] [-o {pretty,file}]
+               {whats-new,latest-versions,download}
+
+Парсер документации Python
+
+positional arguments:
+  {whats-new,latest-versions,download,pep}
+                        Режимы работы парсера
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c, --clear-cache     Очистка кеша
+  -o {pretty,file}, --output {pretty,file}
+                        Дополнительные способы вывода данных
+```
+
+## Cтек проекта
+Python v3.9, BeautifulSoup4
---
+++ src/main.py
@@ -0,0 +1,200 @@
+import logging
+import re
+import requests_cache
+from urllib.parse import urljoin
+
+from bs4 import BeautifulSoup
+from tqdm import tqdm
+
+
+from configs import configure_argument_parser, configure_logging
+from constants import BASE_DIR, MAIN_DOC_URL, PEP_URL, EXPECTED_STATUS
+from outputs import control_output
+from utils import get_response, get_request, find_tag
+
+
+def _output_mismatches_log(non_matching_statuses):
+    """Выводит в лог сообщение с несовпадающими статусами."""
+    message_log = ''
+    if not non_matching_statuses:
+        return
+    for non_matching_status in non_matching_statuses:
+        message_log += ('Несовпадающие статусы:\n'
+                        f'{non_matching_status["url_detail"]}\n'
+                        'Статус в карточке: '
+                        f'{non_matching_status["status_detail"]}\n'
+                        'Ожидаемые статусы: '
+                        f'{non_matching_status["status_table"]}\n')
+    logging.info(message_log)
+
+
+def _forms_result_pep(count_status):
+    """Формирует данные по парсинку PEP для вывода."""
+    results = [('Статус', 'Количество')]
+    total = 0
+    for status, count in count_status.items():
+        results.append((status, count))
+        total += count
+    results.append(('Total', total))
+    return results
+
+
+def pep(session):
+    """Парсит статусы PEP."""
+    response = get_response(session, PEP_URL)
+    if response is None:
+        return
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, features='lxml')
+    section_pep_table = find_tag(soup, 'section',
+                                 attrs={'id': 'numerical-index'})
+    tbody_pep_table = find_tag(section_pep_table, 'tbody')
+    tr_tags_pep_table = tbody_pep_table.find_all('tr')
+    non_matching_statuses = []
+    count_status = {'Active': 0, 'Accepted': 0,
+                    'Deferred': 0, 'Final': 0,
+                    'Provisional': 0, 'Rejected': 0,
+                    'Superseded': 0, 'Withdrawn': 0,
+                    'Draft': 0, 'Active': 0, }
+    for tr_tag_pep_table in tqdm(tr_tags_pep_table):
+        a_tag_pep_table = find_tag(tr_tag_pep_table, 'a', attrs={'class':
+                                   'pep reference internal'})
+        status_code = find_tag(tr_tag_pep_table, 'abbr').text[1:]
+        status_table = EXPECTED_STATUS.get(status_code)
+        url_pep_detail = urljoin(PEP_URL,
+                                 a_tag_pep_table.get('href'))
+        response = get_response(session, url_pep_detail)
+        if response is None:
+            return
+        response.encoding = 'utf-8'
+        soup = BeautifulSoup(response.text, features='lxml')
+        dt_tags_pep_detail = soup.find_all('dt', {'class': 'field-even'})
+        for dt_tag_pep_detail in dt_tags_pep_detail:
+            is_status = dt_tag_pep_detail.find(string='Status')
+            if is_status is None:
+                continue
+            dd_tag_pep_detail = dt_tag_pep_detail.find_next_sibling(
+                'dd', {'class': 'field-even'}
+                )
+            status = dd_tag_pep_detail.text
+            if status in status_table:
+                count_status[status] += 1
+                continue
+            non_matching_statuses.append(
+                {'status_detail': status,
+                    'status_table': status_table,
+                    'url_detail': url_pep_detail}
+                )
+    _output_mismatches_log(non_matching_statuses)
+    return _forms_result_pep(count_status)
+
+
+def whats_new(session):
+    whats_new_url = urljoin(MAIN_DOC_URL, 'whatsnew/')
+    session = requests_cache.CachedSession()
+    results = [('Ссылка на статью', 'Заголовок', 'Редактор, Автор'), ]
+    soup = get_request(session, whats_new_url)
+
+    main_div = find_tag(
+        soup, 'section', attrs={'id': 'what-s-new-in-python'}
+    )
+
+    div_with_ul = find_tag(
+        main_div, 'div', attrs={'class': 'toctree-wrapper'}
+    )
+
+    sections_by_python = div_with_ul.find_all('li',
+                                              attrs={'class': 'toctree-l1'})
+
+    for section in tqdm(sections_by_python):
+        version_a_tag = section.find('a')
+        href = version_a_tag['href']
+        version_link = urljoin(whats_new_url, href)
+        response = get_response(session, version_link)
+        if response is None:
+            continue
+        soup = BeautifulSoup(response.text, features='lxml')
+        h1 = find_tag(soup, 'h1')
+        dl = find_tag(soup, 'dl')
+        dl_text = dl.text.replace('\n', ' ')
+        results.append(
+            (version_link, h1.text, dl_text)
+        )
+    return results
+
+
+def latest_versions(session):
+    session = requests_cache.CachedSession()
+    results = [('Ссылка на документацию', 'Версия', 'Статус')]
+    soup = get_request(session, MAIN_DOC_URL)
+
+    sidebar = soup.find('div', {'class': 'sphinxsidebarwrapper'})
+    ul_tags = sidebar.find_all('ul')
+    for ul in ul_tags:
+        if 'All versions' in ul.text:
+            a_tags = ul.find_all('a')
+            break
+    else:
+        raise Exception('Не найден список c версиями Python')
+
+    pattern = r'Python (?P<version>\d\.\d+) \((?P<status>.*)\)'
+    for a_tag in a_tags:
+        link = a_tag['href']
+        text_match = re.search(pattern, a_tag.text)
+        if text_match is not None:
+            version, status = text_match.groups()
+        else:
+            version, status = a_tag.text, ''
+        results.append(
+            (link, version, status)
+        )
+    return results
+
+
+def download(session):
+    downloads_url = urljoin(MAIN_DOC_URL, 'download.html')
+    session = requests_cache.CachedSession()
+    soup = get_request(session, downloads_url)
+
+    main_tag = soup.find('div', {'role': 'main'})
+    table_tag = main_tag.find('table', {'class': 'docutils'})
+
+    pdf_a4_tag = table_tag.find('a', {'href': re.compile(r'.+pdf-a4\.zip$')})
+    pdf_a4_link = pdf_a4_tag['href']
+    archive_url = urljoin(downloads_url, pdf_a4_link)
+    filename = archive_url.split('/')[-1]
+    downloads_dir = BASE_DIR / 'downloads'
+    downloads_dir.mkdir(exist_ok=True)
+    archive_path = downloads_dir / filename
+    response = session.get(archive_url)
+    with open(archive_path, 'wb') as file:
+        file.write(response.content)
+    logging.info(f'Архив был загружен и сохранён: {archive_path}')
+
+
+MODE_TO_FUNCTION = {
+    'whats-new': whats_new,
+    'latest-versions': latest_versions,
+    'download': download,
+    'pep': pep,
+}
+
+
+def main():
+    configure_logging()
+    logging.info('Парсер запущен!')
+    arg_parser = configure_argument_parser(MODE_TO_FUNCTION.keys())
+    args = arg_parser.parse_args()
+    logging.info(f'Аргументы командной строки: {args}')
+    session = requests_cache.CachedSession()
+    if args.clear_cache:
+        session.cache.clear()
+    parser_mode = args.mode
+    results = MODE_TO_FUNCTION[parser_mode](session)
+    if results is not None:
+        control_output(results, args)
+    logging.info('Парсер завершил работу.')
+
+
+if __name__ == '__main__':
+    main()
